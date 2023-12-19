64cbd96ff785ba8bc5ba1819b5b15006ce7fb445
EHMikel <mikeletxezarraga.info@gmail.com>
2023-12-19 11:03:08 +0100

    Update .gitignore to exclude config.py

---
+++ openai_API/config.py
@@ -0,0 +1,7 @@
+
+OPENAI_API_KEY= "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"
+
+# bvla bala ajdjf adfjaosd hfopashf opasjfoashdfo ashjd foajsodfjasoñdjfoñasdjif oñas jfoñiasdjfo
+#"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" la vieja 
+
+print('soy_retrasado')
